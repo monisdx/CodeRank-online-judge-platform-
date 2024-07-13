59eff0e4d619fb6af9bcce8cf24c8fd68c576972
monisdx <135008013+monisdx@users.noreply.github.com>
2024-07-13 10:38:36 +0530

    navbar and footer done

--- web-client/src/common/Footer.tsx
+++ web-client/src/common/Footer.tsx
@@ -1,7 +1,60 @@
-
-
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <section className="bg-black-8">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p className="caption text-back lg:block">
+          © {new Date().getFullYear()}. All rights reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
+            >
+              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </section>
+
   )
-}
+};
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: "/images/socials/discord.svg",
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: "/images/socials/twitter.svg",
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: "/images/socials/instagram.svg",
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: "/images/socials/telegram.svg",
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: "/images/socials/facebook.svg",
+    url: "#",
+  },
+];
+
+
--- web-client/src/common/Navbar.tsx
+++ web-client/src/common/Navbar.tsx
@@ -6,25 +6,26 @@ export default function Navbar() {
   const [mobileNav, setMobileNav] = useState<boolean>(false);
 
   return (
-    <nav className="p-page w-full flex py-5 justify-between items-center z-[999] bg-black-3 ">
+    <nav className="p-page w-full flex py-5 justify-evenly items-center z-[999] bg-black-8 border-black-6 border-b ">
       <Link to="/" className="flex items-center gap-4 cursor-pointer">
-        <img src={"/images/logo.svg"} alt="logo" className=" w-9 h-9" />
+        <img src={"/images/brain.svg"} alt="logo" className=" w-9 h-9" />
         <p className="text-back font-bold text-lg font-raleway">CodeMaster</p>
       </Link>
-      <div className="flex gap-10">
-        <ul className="list-none widescreen:flex hidden justify-end items-center gap-x-8 flex-1">
+      
+        <div className="list-none widescreen:flex hidden justify-center items-center gap-x-20 flex-1">
           {navLinks.map((item, key) => (
             <Link
               key={key}
               to={item.to}
-              className={`font-cabin font-medium cursor-pointer text-md text-back ${key === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+              className={`font-cabin font-medium cursor-pointer text-md text-back/50 hover:text-back duration-300`}
             >
               {item.title}
             </Link>
           ))}
-        </ul>
-       
-      </div>
+        </div>
+        <button type="button" className="widescreen:flex hidden px-5 py-2 font-medium text-md font-cabin rounded-3xl text-black-1 bg-back outline-none border-none">
+         Sign In
+        </button>
       <div className="widescreen:hidden flex flex-1 justify-end items-center">
       <button
             className="relative"
@@ -35,17 +36,20 @@ export default function Navbar() {
         <div
           className={`${mobileNav ? "flex" : "hidden"} p-6 bg-black-3 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar border-black-2 border-2`}
         >
-          <ul className="list-none flex flex-col justify-end items-center flex-1">
+          <div className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((item, key) => (
               <Link
                 key={key}
                 to={item.to}
-                className={`font-cabin font-medium cursor-pointer text-md text-back ${key === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                className={`font-cabin font-medium cursor-pointer text-md text-back/50 hover:text-back mb-4 duration-300`}
               >
                {item.title}
               </Link>
             ))}
-          </ul>
+            <button type="button" className="flex px-5 py-2 font-medium font-cabin text-md rounded-3xl text-black-1 bg-back outline-none border-none">
+              Sign In
+            </button>
+          </div>
         </div>
       </div>
     </nav>
