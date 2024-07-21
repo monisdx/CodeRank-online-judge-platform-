b222c96c6895b0f55312f5fe8f0b991940a25623
monisdx <135008013+monisdx@users.noreply.github.com>
2024-07-21 17:08:49 +0530

    fix error 500 on compiler

--- server/src/utils/executeCode.ts
+++ server/src/utils/executeCode.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { exec } from "child_process";
-import { stderr } from "process";
 
 const dirOutputs = path.join(__dirname, "outputs");
 
@@ -10,14 +11,17 @@ if (!fs.existsSync(dirOutputs)) {
 }
 
 export const executeCode = async (filePath: string) => {
+  console.log("filepath: ", filePath);
   const codeId = path.basename(filePath).split(".")[0];
-  const output_filename = `${codeId}.out`;
+  // const output_filename = `${codeId}.out`;
 
-  const outputPath = path.join(dirOutputs, output_filename);
+  const outputPath = path.join(dirOutputs, `${codeId}.exe`);
+  console.log("outputpath: ", outputPath);
+  // console.log(outputPath);
 
   return new Promise((resolve, reject) => {
     exec(
-      `g++ ${filePath} -o ${outputPath} && cd ${dirOutputs} && ./${output_filename}`,
+      `g++ ${filePath} -o ${outputPath} && cd ${dirOutputs} && .\\${codeId}.exe`,
       (error, stdout, stderr) => {
         if (error) {
           reject({ error, stderr });
