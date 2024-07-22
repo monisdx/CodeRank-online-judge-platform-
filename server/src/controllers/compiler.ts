09eaa24d0413cb6cc56337eb4b792938ad86214b
monisdx <135008013+monisdx@users.noreply.github.com>
2024-07-22 18:01:16 +0530

    add testcase feature in compiler

--- server/src/controllers/compiler.ts
+++ server/src/controllers/compiler.ts
@@ -19,3 +19,37 @@ export const runCode = async (req: Request, res: Response) => {
     res.status(500).json({ message: err });
   }
 };
+
+export const submitCode = async (req: Request, res: Response) => {
+  const { language = "cpp", code, testcases } = req.body;
+
+  if (!code)
+    return res.status(500).json({ success: false, message: "Code is empty" });
+
+  try {
+    const filePath = await generateFile(language, code);
+    let index = 1;
+    for (const { input, expectedoutput } of testcases) {
+      const input_filePath = await generateInputFile(input);
+      const output = await executeCode(filePath, input_filePath);
+
+      console.log("input: ", input);
+      console.log("output: ", output);
+
+      const iscorrect: boolean = output === expectedoutput;
+
+      if (!iscorrect) {
+        return res.json({
+          output,
+          verdict: `wrong answer on testcase ${index}`,
+        });
+      }
+
+      index++;
+    }
+
+    res.json({ verdict: "accepted" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- server/src/routes/compiler.ts
+++ server/src/routes/compiler.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { runCode } from "../controllers/compiler";
+import { runCode, submitCode } from "../controllers/compiler";
 
 const router = express.Router();
 
 router.post("/run", runCode);
+router.post("/submit", submitCode);
 
 export default router;
